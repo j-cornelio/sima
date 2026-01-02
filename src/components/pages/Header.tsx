5c288db2438b1ee75a188f41302e9459d359e83f
Julio <empjulio@gmail.com>
2026-01-02 11:46:13 -0400

    test header

--- src/components/pages/Header.tsx
+++ src/components/pages/Header.tsx
@@ -35,7 +35,7 @@ function Header () {
                         </li>
                         <li className="nav-item">
                             <Link  className="nav-link" to="/contact">
-                                Contacto
+                                Contact
                             </Link>
                         </li>
                     </ul>
