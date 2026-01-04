7e172116f22ea963d6c74a34780f3514188b63df
Julio <empjulio@gmail.com>
2026-01-04 17:46:37 -0400

    add EL

--- src/components/pages/Contact.tsx
+++ src/components/pages/Contact.tsx
@@ -37,7 +37,7 @@ function Contact () {
       <Header />
       <div id="contact" className="container">
           <div className="livingroom">
-            <h1>Mejor Servicio Inmobilario en Santo Domingo</h1>
+            <h1>El Mejor Servicio Inmobilario en Santo Domingo</h1>
             <h2>¡Póngase en contacto con nosotros para obtener asesoramiento inmobiliario experto y un servicio personalizado!</h2>
           </div>
           {/*onSubmit={handleSubmit}*/}
