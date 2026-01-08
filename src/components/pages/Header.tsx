3f09995c7ca043168f9d100edce945f922335c5e
Julio <empjulio@gmail.com>
2026-01-08 16:31:50 -0400

    userlist link

--- src/components/pages/Header.tsx
+++ src/components/pages/Header.tsx
@@ -38,6 +38,11 @@ function Header () {
                                 Contacto
                             </Link>
                         </li>
+                        <li className="nav-item">
+                            <Link  className="nav-link" to="/userlist">
+                                userlist
+                            </Link>
+                        </li>
                     </ul>
                 </div>
                     <div className="gtranslate_wrapper"></div>
