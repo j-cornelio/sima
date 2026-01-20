a4853223d475dcd7043cbe5cc6d8be94c5beeb04
Julio <empjulio@gmail.com>
2026-01-20 18:54:28 -0400

    contact api on url

--- src/components/pages/Contact.tsx
+++ src/components/pages/Contact.tsx
@@ -33,7 +33,7 @@ function Contact () {
         }
       */}
           
-          axios.post('https://sima-rd.net/signup', values)
+          axios.post('https://sima-rd.net/api/signup', values)
               .then(res => console.log("Registered SUCESSFULLY"))
               .catch((err) => console.log('error: ', err))
     }
@@ -43,7 +43,7 @@ function Contact () {
       <Header />
       <div id="contact" className="container">
           <div className="livingroom">
-            <h1>Hey. Mejores Servicios Inmobilario en Santo Domingo.</h1>
+            <h1>KLK. Mejores Servicios Inmobilario en Santo Domingo.</h1>
             <h2>¡Póngase en contacto con nosotros para obtener asesoramiento inmobiliario experto y un servicio personalizado!</h2>
           </div>
           
