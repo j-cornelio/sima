309a11bada9bd806e635d845802ef6e2078258c7
Julio <empjulio@gmail.com>
2026-02-04 20:07:32 -0400

    added back stylesheet for carousel, added data for Paseo 200

--- src/components/pages/sales/Paseo200.tsx
+++ src/components/pages/sales/Paseo200.tsx
@@ -6,6 +6,11 @@ import image2 from '../../../images/sales/paseo_200/2.jpeg'
 import image3 from '../../../images/sales/paseo_200/3.jpeg'
 import image4 from '../../../images/sales/paseo_200/4.jpeg'
 import image5 from '../../../images/sales/paseo_200/5.jpeg'
+import image6 from '../../../images/sales/paseo_200/6.jpeg'
+import image7 from '../../../images/sales/paseo_200/7.jpeg'
+import image8 from '../../../images/sales/paseo_200/8.jpeg'
+import image9 from '../../../images/sales/paseo_200/9.jpeg'
+import image10 from '../../../images/sales/paseo_200/10.jpeg'
 
 function Paseo200 () {
     return ( 
@@ -26,24 +31,44 @@ function Paseo200 () {
                     <li data-target="#myCarousel" data-slide-to="2"></li>
                     <li data-target="#myCarousel" data-slide-to="3"></li>
                     <li data-target="#myCarousel" data-slide-to="4"></li>
+                    <li data-target="#myCarousel" data-slide-to="5"></li>
+                    <li data-target="#myCarousel" data-slide-to="6"></li>
+                    <li data-target="#myCarousel" data-slide-to="7"></li>
+                    <li data-target="#myCarousel" data-slide-to="8"></li>
+                    <li data-target="#myCarousel" data-slide-to="9"></li>
                 </ol>
 
                 {/* Wrapper for slides */}
                 <div className="carousel-inner">
                     <div className="item">
-                        <div className="sales-img" style={{backgroundImage:  "url(" + image1}}></div>
+                        <div className="sales-img" style={{backgroundPosition: "25% 50%", backgroundImage:  "url(" + image1}}></div>
                     </div>
                     <div className="item active">
-                        <div className="sales-img" style={{backgroundImage:  "url(" + image2}}></div>
+                        <div className="sales-img" style={{backgroundPosition: "25% 50%", backgroundImage:  "url(" + image2}}></div>
                     </div>
                     <div className="item">
-                        <div className="sales-img" style={{backgroundImage:  "url(" + image3}}></div>
+                        <div className="sales-img" style={{backgroundPosition: "25% 50%", backgroundImage:  "url(" + image3}}></div>
                     </div>
                     <div className="item">
-                        <div className="sales-img" style={{backgroundImage:  "url(" + image4}}></div>
+                        <div className="sales-img" style={{backgroundPosition: "25% 50%", backgroundImage:  "url(" + image4}}></div>
                     </div>
                     <div className="item">
-                        <div className="sales-img" style={{backgroundImage:  "url(" + image5}}></div>
+                        <div className="sales-img" style={{backgroundPosition: "25% 50%", backgroundImage:  "url(" + image5}}></div>
+                    </div>
+                    <div className="item">
+                        <div className="sales-img" style={{backgroundPosition: "25% 50%", backgroundImage:  "url(" + image6}}></div>
+                    </div>
+                    <div className="item">
+                        <div className="sales-img" style={{backgroundPosition: "25% 50%", backgroundImage:  "url(" + image7}}></div>
+                    </div>
+                    <div className="item">
+                        <div className="sales-img" style={{backgroundPosition: "25% 50%", backgroundImage:  "url(" + image8}}></div>
+                    </div>
+                    <div className="item">
+                        <div className="sales-img" style={{backgroundPosition: "25% 50%", backgroundImage:  "url(" + image9}}></div>
+                    </div>
+                    <div className="item">
+                        <div className="sales-img" style={{backgroundPosition: "25% 50%", backgroundImage:  "url(" + image10}}></div>
                     </div>
                 </div>
 
@@ -62,31 +87,38 @@ function Paseo200 () {
             </div>
             <div className="row">
                 <div className="col-md-6" style={{width: "70%"}}>
-                    <h1 className="price">Precio: $335,000 negociable</h1>
+                    <h1 className="price">Precio: $460,000 negociable</h1>
                     <div className="icons">
-                        <p><i className="fa-solid fa-bed"></i> <span>2/3 Habitaciones</span></p>
-                        <p><i className="fa-solid fa-bath"></i> <span>3.5 Baños</span></p>
-                        <p><i className="fa-solid fa-car"></i> <span>3 Estacionamientos</span></p>
-                        <p><i className="fa-solid fa-ruler"></i> <span>165 Mt2</span></p>
+                        <p><i className="fa-solid fa-bed"></i> <span>3 Habitaciones</span></p>
+                        <p><i className="fa-solid fa-bath"></i> <span>4.5 Baños</span></p>
+                        <p><i className="fa-solid fa-car"></i> <span>4 Parqueos</span></p>
+                        <p><i className="fa-solid fa-ruler"></i> <span>200 Mt2</span></p>
                     </div>
 
                     <h2>Reflejos:</h2>
                     <ul>
-                        <li>2 habitaciones con opción a 3</li>
                         <li>Cuarto de servicio</li>
+                        <li>Sala</li>
                         <li>Comedor</li>
                         <li>Cocina</li>
-                        <li>Balcón doble</li>
-                        <li>Sala</li>
+                        <li>Estudio</li>
+                        <li>Balcón amplio</li>
+                        <li>Aires Acond. Inverter</li>
+                        <li>extractor de grasa</li>
+                        <li>shuters</li>
+                        <li>perma eléctrica</li>
+                        <li>luces domotica</li>
+ 
                         <li><strong>Áreas sociales</strong></li>
+
                         <li>Terrazas</li>
                         <li>Parque</li>
+                        <li>Gimnasio</li>
                         <li>2 salones de eventos</li>
                         <li>Area de niños</li>
-                        <li>Gimnasio</li>
                         <li>Planta full</li>
-                        <li>Mayas en balcón y ventanas</li>
-                        <li>Aires Acond Sala y habitaciones</li>
+                        <li>Cuarto de choferes</li>
+                        <li>Lobby climatizado</li>
                     </ul>
                 </div>
                 <div className="col-md-6 contact" style={{width: "30%", textAlign: "center"}}>
