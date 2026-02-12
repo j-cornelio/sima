53630f402bdba1869737f0f3c133283eb0378c48
Julio <empjulio@gmail.com>
2026-02-12 13:16:07 -0400

    NEW navigation on header

--- src/components/pages/Header.tsx
+++ src/components/pages/Header.tsx
@@ -15,36 +15,33 @@ function Header () {
     }, [])
 
     return (
-          <nav id="header" className="navbar navbar-expand-lg navbar-dark">
-            <div className="container-fluid">
-                <Link className="navbar-brand" to="/">
-                    <img src={logo} width="120px" />
-                </Link>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                   <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarNav">
-                    <ul className="navbar-nav">
-                        <li className="nav-item"> 
-                            <Link  className="nav-link" to="/sales">
-                                Ventas
-                            </Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link  className="nav-link" to="/rentals">
-                                Alquileres
-                            </Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link  className="nav-link" to="/contact">
-                                Contacto
-                            </Link>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-            <div className="gtranslate_wrapper"></div>  
-        </nav>
+      <nav>
+        <div className="wrapper">
+          <div className="logo">
+             <Link className="navbar-brand" to="/">
+                <img src={logo} width="120px" />
+            </Link>
+          </div>
+          <input type="radio" name="slider" id="menu-btn" />
+          <input type="radio" name="slider" id="close-btn" />
+          <ul className="nav-links">
+            <label htmlFor="close-btn" className="btn close-btn"><i className="fas fa-times"></i></label>
+            <li>
+              <Link  className="nav-link" to="/sales">
+                Ventas
+            </Link>
+            </li>
+            <li>
+               <Link  className="nav-link" to="/rentals">
+                  Alquileres
+              </Link>
+            </li>
+            
+            <li><div className="gtranslate_wrapper"></div></li>
+          </ul>
+          <label htmlFor="menu-btn" className="btn menu-btn"><i className="fas fa-bars"></i></label>
+        </div>
+      </nav>
     )
 }
 
